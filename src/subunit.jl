aeb2e74ea35db43cbef683d6a04ba12a940ae05c
Michael Wang <michael.wang.physics@gmail.com>
2022-06-10 20:02:15 -0400

    Basic setup

---
+++ src/subunit.jl
@@ -0,0 +1,15 @@
+export BindingSite
+export Subunit
+export Bond
+
+mutable struct BindingSite
+    position::SVector{3, Float64}
+end
+
+mutable struct Subunit
+    
+end
+
+mutable struct Bond
+
+end
---
+++ src/system.jl
@@ -0,0 +1,5 @@
+export System
+
+mutable struct System
+
+end

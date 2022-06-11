5ce441c8f34880a72bbc3df21330d18b24a3e6c9
Michael Wang <michael.wang.physics@gmail.com>
2022-06-11 15:47:27 -0400

    Minor fix

--- src/system.jl
+++ src/system.jl
@@ -26,7 +26,7 @@ function initialize_lattice(unit_cell::Vector{Subunit}, lattice_vectors::NTuple{
 
     return subunits
 end
-initialize_lattice(unit_cell::Vector{Subunit}, lattice_vectors::NTuple{2, Vector{Float64}}, lattice_dimensions::NTuple{2, Int64}) = initialize_lattice(unit_cell, (lattice_vectors[1], lattice_vector[2], [0.0, 0.0, 1.0]), (lattice_dimensions[1], lattice_dimensions[2], 1))
+initialize_lattice(unit_cell::Vector{Subunit}, lattice_vectors::NTuple{2, Vector{Float64}}, lattice_dimensions::NTuple{2, Int64}) = initialize_lattice(unit_cell, (lattice_vectors[1], lattice_vectors[2], [0.0, 0.0, 1.0]), (lattice_dimensions[1], lattice_dimensions[2], 1))
 
 function link(subunits::Vector{Subunit}, interactions::Vector{Tuple{Int64, Int64, I}}, neighbor_cutoff::Float64, bond_cutoff::Float64) where I <: Interaction
     bonds = Vector{Bond}()

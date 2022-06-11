4e657bfd76a0ae66d0bb0c9088c4f06e2cc599e6
Michael Wang <michael.wang.physics@gmail.com>
2022-06-11 16:35:08 -0400

    Added noise to GradientDescent

--- src/integrator.jl
+++ src/integrator.jl
@@ -1,23 +1,28 @@
-export TemperatureQuench
+export GradientDescent
 
-mutable struct TemperatureQuench
+mutable struct GradientDescent
     subunits::Vector{Subunit}
 
     step_size::Float64
 
-    T::Float64
-    ΔT::Float64
+    ampl::Float64
+    Δampl::Float64
     quench_duration::Int64
 end
 
-function TemperatureQuench(subunits::Vector{Subunit}, step_size::Float64, T_start::Float64, quench_duration::Int64)
-    ΔT = T_start / quench_duration
+function GradientDescent(subunits::Vector{Subunit}, step_size::Float64, ampl_start::Float64, quench_duration::Int64)
+    Δampl = ampl_start / quench_duration
 
-    return TemperatureQuench(subunits, step_size, T_start, ΔT, quench_duration)
+    return GradientDescent(subunits, step_size, ampl_start, Δampl, quench_duration)
+end
+
+function GradientDescent(subunits::Vector{Subunit}, step_size::Float64)
+    return GradientDescent(subunits, step_size, 0.0, 0.0, 0)
 end
 
 function update_subunits!(integrator::TemperatureQuench)
     δ = integrator.step_size
+    ampl = integrator.ampl
     Threads.@threads for subunit in integrator.subunits
         sub_pos = subunit.position
         
@@ -27,9 +32,9 @@ function update_subunits!(integrator::TemperatureQuench)
             site_pos = site.position
             sf = site.force
             
-            fx += sf[1]
-            fy += sf[2]
-            fz += sf[3]
+            fx += sf[1] + (ampl > 0.0 ? ampl * randn() : 0.0)
+            fy += sf[2] + (ampl > 0.0 ? ampl * randn() : 0.0)
+            fz += sf[3] + (ampl > 0.0 ? ampl * randn() : 0.0)
             
             rx, ry, rz = site_pos[1] - sub_pos[1], site_pos[2] - sub_pos[2], site_pos[3] - sub_pos[3]
             τx += ry * sf[3] - sf[2] * rz 
@@ -43,4 +48,5 @@ function update_subunits!(integrator::TemperatureQuench)
             rotate!(subunit, τx / τ, τy / τ, τz / τ, δ * τ)
         end
     end
+    integrator.ampl = (ampl > 0.0 ? ampl - integrator.Δampl : 0.0)
 end

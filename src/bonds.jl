8b533cee4a44f75baf3f695bb81341391389d084
Michael Wang <michael.wang.physics@gmail.com>
2022-06-14 20:13:15 -0400

    Fixing type issues with bonds...hopefully

--- src/bonds.jl
+++ src/bonds.jl
@@ -31,39 +31,41 @@ mutable struct Bond{I <: Interaction}
     interaction::I
 end
 
-function compute_forces!(bonds::Vector{Bond{Harmonic}})
-    Threads.@threads for bond in bonds
-        site1, site2 = bond.pair
-        pos1, pos2 = site1.position, site2.position
-        Δx, Δy, Δz = pos1[1] - pos2[1], pos1[2] - pos2[2], pos1[3] - pos2[3]
+function compute_forces!(bond::Bond{Harmonic})
+    site1, site2 = bond.pair
+    pos1, pos2 = site1.position, site2.position
+    Δx, Δy, Δz = pos1[1] - pos2[1], pos1[2] - pos2[2], pos1[3] - pos2[3]
 
+    f1, f2 = site1.force, site2.force
+    k = bond.interaction.k
+    f2[1] = -(f1[1] = -k * Δx)
+    f2[2] = -(f1[2] = -k * Δy)
+    f2[3] = -(f1[3] = -k * Δz)
+
+    site1.energy = site2.energy = 0.5 * k * (Δx^2 + Δy^2 + Δz^2)
+end
+
+function compute_forces!(bond::Bond{LennardJones})
+    site1, site2 = bond.pair
+    pos1, pos2 = site1.position, site2.position
+    Δx, Δy, Δz = pos1[1] - pos2[1], pos1[2] - pos2[2], pos1[3] - pos2[3]
+    Δr² = Δx^2 + Δy^2 + Δz^2
+
+    if Δr² < bond.cutoff^2
         f1, f2 = site1.force, site2.force
-        k = bond.interaction.k
-        f2[1] = -(f1[1] = -k * Δx)
-        f2[2] = -(f1[2] = -k * Δy)
-        f2[3] = -(f1[3] = -k * Δz)
 
-        site1.energy = site2.energy = 0.5 * k * (Δx^2 + Δy^2 + Δz^2)
+        invΔr² = bond.interaction.σ^2 / Δr^2
+        invΔr⁶ = invΔr²^3
+        coef = bond.interaction.ϵ * (48.0 * invΔr⁶ - 24.0) * invΔr⁶ / Δr²
+
+        f2[1] = -(f1[1] = coef * Δx)
+        f2[2] = -(f1[2] = coef * Δy)
+        f2[3] = -(f1[3] = coef * Δz)
     end
 end
 
-function compute_forces!(bonds::Vector{Bond{LennardJones}})
+function compute_forces!(bonds::Vector{Bond})
     Threads.@threads for bond in bonds
-        site1, site2 = bond.pair
-        pos1, pos2 = site1.position, site2.position
-        Δx, Δy, Δz = pos1[1] - pos2[1], pos1[2] - pos2[2], pos1[3] - pos2[3]
-        Δr² = Δx^2 + Δy^2 + Δz^2
-
-        if Δr² < bond.cutoff^2
-            f1, f2 = site1.force, site2.force
-
-            invΔr² = bond.interaction.σ^2 / Δr^2
-            invΔr⁶ = invΔr²^3
-            coef = bond.interaction.ϵ * (48.0 * invΔr⁶ - 24.0) * invΔr⁶ / Δr²
-
-            f2[1] = -(f1[1] = coef * Δx)
-            f2[2] = -(f1[2] = coef * Δy)
-            f2[3] = -(f1[3] = coef * Δz)
-        end
+        compute_forces!(bond)
     end
 end

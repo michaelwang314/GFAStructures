03e1a2ea25cd8d03a7fb6dd78ab4dc1ea8a238f1
Michael Wang <michael.wang.physics@gmail.com>
2022-06-19 16:53:15 -0400

    Added checker to make sure that find_neighbors returns proper neighbors

--- src/system.jl
+++ src/system.jl
@@ -44,6 +44,23 @@ function find_neighbors(subunits::Vector{RigidSubunit}, subunit_cutoff::Float64,
     return interaction_sites, neighbors
 end
 
+function check_neighbors(neighbors::Vector{Vector{InteractionSite}})
+    sizes = unique!(length.(neighbors))
+    counts = Dict{Int64, Int64}()
+    for size in sizes
+        if haskey(counts, size)
+            counts[size] += 1
+        else
+            counts[size] = 1
+        end
+    end
+
+    println("There are $(length(neighbors)) interaction sites with neighbors")
+    for (size, count) in counts
+        println("    $count have $size neighbors")
+    end
+end
+
 function get_energy(subunits::Vector{RigidSubunit})
     energy = 0.0
     for subunit in subunits

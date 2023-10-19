a96d796da33cbd23df20267c74189af1b09be804
Michael Wang <michael.wang.physics@gmail.com>
2023-10-19 13:51:55 -0400

    minor change

--- src/external_forces.jl
+++ src/external_forces.jl
@@ -3,20 +3,15 @@ abstract type ExternalForce end
 struct AnisotropicSpring <: ExternalForce
     sites::Vector{InteractionSite}
 
-    kx::Float64
-    ky::Float64
-    kz::Float64
-
-    x0::Float64
-    y0::Float64
-    z0::Float64
+    k::MVector{3, Float64}
+    r0::MVector{3, Float64}
 end
 
 function compute_forces!(as::AnisotropicSpring)
     for site in as.sites
-        site.force[1] += -as.kx * (site.position[1] - as.x0)
-        site.force[2] += -as.ky * (site.position[2] - as.y0)
-        site.force[3] += -as.kz * (site.position[3] - as.z0)
+        site.force[1] += -as.k[1] * (site.position[1] - as.r0[1])
+        site.force[2] += -as.k[2] * (site.position[2] - as.r0[2])
+        site.force[3] += -as.k[3] * (site.position[3] - as.r0[3])
     end
 end
 
